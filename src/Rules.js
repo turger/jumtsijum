fde1822273f548182f2c1118e33944b6949c0b78
Taru Airola <taru.airola@gmail.com>
2021-09-24 18:21:15 +0300

    Update rules

--- src/Rules.js
+++ src/Rules.js
@@ -4,13 +4,13 @@ const Rules = () =>
   <ol>
     <li>Joukkueet valitsevat vuorotellen numerokortteja, joiden perusteella pelinjohtaja kääntää lauluun liittyviä sanoja esille yksi kerrallaan.</li>
     <li>Jos esille käännetty sana on punainen, vuoro siirtyy automaattisesti toiselle joukkueelle.</li>
-    <li>Vuoron alussa voi valita avaako luukun vai arvaako joukkue laulun ilman luukun avaamista.</li>
+    <li>Vuoron alussa joukkue valitsee avaako seuraavan luukun vai arvaako joukkue laulun jo näkyvien sanojen avulla.</li>
     <li>Joukkueet yrittävät keksiä, mikä laulu on kyseessä ja vuorossa oleva joukkue saa arvata ensin.</li>
-    <li>Jos joukkue arvaa väärin, mutta laulunäytteessä on oikeassa järjestyksessä kaikki näkyvät sanat, niin joukkue saa jatkaa. Muutoin vuoro siirtyy toiselle joukkueelle.</li>
-    <li>Mikäli joukkue ei pysty muodostamaan sanoista laulua, siirtyy vuoro toiselle joukkueelle.</li>
-    <li>Pisteen saa se joukkue, joka ensimmäisenä arvaa oikein esittäjän ja laulun nimen ja laulaa oikean kohdan laulusta.</li>
+    <li>Jos joukkue arvaa laulun väärin, mutta on ennen arvaamista avannut luukun ja luukusta paljastunut sana (tai sen taivutettu muoto) esiintyy laulussa, niin joukkue saa jatkaa. Muutoin vuoro siirtyy toiselle joukkueelle.</li>
+    <li>Mikäli joukkue ei pysty muodostamaan sanasta laulua, siirtyy vuoro toiselle joukkueelle.</li>
+    <li>Pisteen saa se joukkue, joka ensimmäisenä laulaa oikean laulun ja oikean kohdan laulusta.</li>
     <li>Kun kappale on arvattu, siirrytään seuraavaan lauluun.</li>
-    <li>Voittaja on se joukkue, jolla on ensimmäisenä viisi pistettä.</li>
+    <li>Voittaja on se joukkue, jolla on pelin lopuksi enemmän pisteitä.</li>
   </ol>
 
 export default Rules

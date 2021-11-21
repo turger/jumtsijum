165ff0eb4d678e6df8bd633e332dd18917a0c046
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2021-11-21 14:01:03 +0200

    Add routing & placeholders for Settings

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Game from './Game'
 import GameSelection from './GameSelection'
 import GameMaster from './GameMaster'
+import Settings from './Settings'
 
 const Main = () => (
   <main>
     <Router>
       <Switch>
+        <Route path='/settings' component={Settings}/>
         <Route exact path='/' component={GameSelection}/>
         <Route exact path='/:gameId' component={Game}/>
         <Route exact path='/master/:gameId' component={GameMaster}/>
---
+++ src/Settings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Settings.css'
+
+const Settings = () => (
+    <p>Settings</p>
+)
+
+export default Settings

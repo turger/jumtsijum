cd01ef27bef4a7bedf10e0e5e95ca70340788e9b
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2021-11-21 14:45:53 +0200

    Nested routing

--- src/Settings.js
+++ src/Settings.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom'
 
 import './Settings.css'
 
 const Settings = () => (
-    <p>Settings</p>
+    <Router>
+        <SettingsNav />
+        <Switch>
+            <Route exact path='/settings/songs' component={Songs} />
+            <Route exact path='/settings/playlists' component={Playlists} />
+            <Redirect from='/settings' to='/settings/songs' />
+        </Switch>
+    </Router>
+)
+
+
+const SettingsNav = () => (
+    <div className='navWrapper'>
+        <NavItem label='Songs' route='/settings/songs'></NavItem>
+        <NavItem label='Playlists' route='/settings/playlists'></NavItem>
+    </div>
+)
+
+const NavItem = ({ label, route }) => (
+    <Link to={route}>{label}</Link>
+)
+
+const Songs = () => (
+    <p>Songs</p>
+)
+
+const Playlists = () => (
+    <p>Playlists</p>
 )
 
 export default Settings

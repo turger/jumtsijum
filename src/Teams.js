e099db652c59b056a4e5c9496803918600c92075
Taru Airola <taru.airola@wunderdog.fi>
2023-10-30 20:03:24 +0200

    Refactor Teams class component to functional component with hooks

--- src/Teams.js
+++ src/Teams.js
@@ -1,84 +1,66 @@
-import React, { Component, Fragment } from 'react'
-import _ from 'lodash'
-
-import { getOneGame, getTeamsRef, updatePoints } from './services/firebase'
-
+import React, {useEffect, useRef, useState} from 'react'
+import {getOneGame, getTeamsRef, updatePoints} from './services/firebase'
 import './Teams.css'
 
-class Teams extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      points: { red: null, blue: null }
-    }
-    this._pointsRef = null
-  }
+const Teams = ({gameId, buttonsDisabled}) => {
+  const initialPoints = {red: null, blue: null}
 
-  async componentDidMount() {
-    const { gameId } = this.props
-    const game = await getOneGame(gameId)
-    this.setState({
-      points: {
+  const [points, setPoints] = useState(initialPoints)
+  const pointsRef = useRef(null)
+
+  useEffect(() => {
+    const getGamePoints = async () => {
+      const game = await getOneGame(gameId)
+      setPoints({
         red: game.teams.red.points,
         blue: game.teams.blue.points,
-      }
-    })
-    // set listener to points statuses
-    this._pointsRef = getTeamsRef(gameId)
-    await this._pointsRef.on('value', (snap) => {
-      const teams = snap.val() ? snap.val() : null
-      this.setState({
-        points: {
+      })
+    }
+
+    getGamePoints()
+  }, [gameId]);
+
+  useEffect(() => {
+    const setListenerToPoints = async () => {
+      pointsRef.current = getTeamsRef(gameId)
+      await pointsRef.current.on('value', (snap) => {
+        const teams = snap.val() ? snap.val() : null
+        setPoints({
           red: teams.red.points,
           blue: teams.blue.points,
-        }
+        })
       })
+    }
+    setListenerToPoints()
+  }, [gameId]);
+
+  const updateTeamPoints = (team, points) => {
+    setPoints((previousPoints) => {
+      updatePoints(gameId, team, previousPoints[team] + points)
+      return {...previousPoints, ...{[team]: previousPoints[team] + points}}
     })
   }
 
-  componentWillUnmount() {
-    if (this._pointsRef) this._pointsRef.off()
-  }
+  const teamPoints = team =>
+    <>
+      {!buttonsDisabled && <button onClick={() => updateTeamPoints(team, -1)}>-</button>}
+      <div>{points[team] || 0}</div>
+      {!buttonsDisabled && <button onClick={() => updateTeamPoints(team, 1)}>+</button>}
+    </>
 
-  addPoint = team => {
-    const teamPoints = (_.get(this.state.points, team) || 0) + 1
-    let points = this.state.points
-    points[team] = teamPoints
-    this.setState(points)
-    updatePoints(this.props.gameId, team, teamPoints)
-  }
-
-  reducePoint = team => {
-    const teamPoints = (_.get(this.state.points, team) || 0) - 1
-    let points = this.state.points
-    points[team] = teamPoints
-    this.setState(points)
-    updatePoints(this.props.gameId, team, teamPoints)
-  }
-
-  render() {
-    const { buttonsDisabled } = this.props
-
-    const points = team =>
-      <Fragment>
-        {!buttonsDisabled && <button onClick={() => this.reducePoint(team)}>-</button>}
-        <div>{this.state.points[team] || 0}</div>
-        {!buttonsDisabled && <button onClick={() => this.addPoint(team)}>+</button>}
-      </Fragment>
-
-    return (
-      <div className='Teams'>
-        <div className='Team Team--blue'>
-          <div className='Team__title'>Sininen tiimi</div>
-          <div className='Team__points'>{points('blue')}</div>
-        </div>
-        <div className='Team Team--red'>
-          <div className='Team__title'>Punainen tiimi</div>
-          <div className='Team__points'>{points('red')}</div>
-        </div>
+  return (
+    <div className='Teams'>
+      <div className='Team Team--blue'>
+        <div className='Team__title'>Sininen tiimi</div>
+        <div className='Team__points'>{teamPoints('blue')}</div>
       </div>
-    )
-  }
+      <div className='Team Team--red'>
+        <div className='Team__title'>Punainen tiimi</div>
+        <div className='Team__points'>{teamPoints('red')}</div>
+      </div>
+    </div>
+  )
+
 }
 
 export default Teams
